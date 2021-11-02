6897a9bc87927c91d00be104a4da102acd5c9aee
snovaisg <snovaisg.97@gmail.com>
2021-11-02 11:59:35 +0000

    improved data structure and added Logits2Predictions

---
+++ Logits2Predictions.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+class Logits2Predictions():
+    """
+    This class transforms logit matrices (outputs of a model)
+    into predictions based on a prediction method, eg: topk-based, threshold-based,etc...
+    
+    Receives dataframes as input and outputs a prediction daframe of same shape
+    """
+    def __init__(self,logits):
+        """
+        logits can be: dataframe (tensor to be supported)
+        """
+        self.logits = logits
+    
+    def topk(self,k:int=30):
+        """
+        k : int
+            recall@k
+        """
+        temp = self.logits.copy().filter(like='diag_')
+        
+        # get k-largest logit of each example
+        temp['cutoff'] = temp.apply(lambda row: row.nlargest(k).iloc[-1],axis=1)
+        
+        prediction_data = temp.apply(lambda row: np.where(row >= row.cutoff,1,0),axis=1).tolist()
+        
+        prediction_df = pd.DataFrame(index=temp.index,
+                                     data=prediction_data,
+                                     columns=temp.columns
+                                    ).drop(columns=['cutoff'])
+        
+        return prediction_df
